fb774ebc4d8e76f1919ec2771f4d727a024088cb
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-07-29 03:27:12 +0500

    Add Jsx Concept File

---
+++ react-app/src/Components/Home.js
@@ -0,0 +1,21 @@
+// src/components/Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Import the CSS file for Home
+import logo from '../logo.svg'; // Adjust the path as needed
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="React logo" />
+        <h5 className='Home-h5'>./src/home.js</h5>
+        <Link to="/my-jsx" className="Home-link">
+          Learn React with Muntazer
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
---
+++ react-app/src/Components/Jsx.js
@@ -0,0 +1,12 @@
+// src/components/Jsx.js
+import React from 'react';
+
+function JsxConcept() {
+  return (
+    <div>
+      <h1>This is the JSX Concept page</h1>
+    </div>
+  );
+}
+
+export default JsxConcept;
