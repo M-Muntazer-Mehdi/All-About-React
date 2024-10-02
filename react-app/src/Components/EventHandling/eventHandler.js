bb8b3f93025e5b86b962528612a9606c32f4ace4
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-02 21:01:24 +0500

    Event Handler : EventObject Example

--- react-app/src/Components/EventHandling/eventHandler.js
+++ react-app/src/Components/EventHandling/eventHandler.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PassingFunction from './passingFunctionAsEventHandler';
 import Inline from "./inlineEventHandler";
+import Object from "./eventObject";
 
 function EventHandler (){
     return(
         <div>
             <PassingFunction />
             <Inline />
+            <Object />
         </div>
     );
 }
