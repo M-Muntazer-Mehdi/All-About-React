c366b254593e43c6a3b2fc6aec0d6993829d7e92
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-02 20:03:03 +0500

    State Management : UseReducer Example

--- react-app/src/Components/stateManagement/stateMainFile.js
+++ react-app/src/Components/stateManagement/stateMainFile.js
@@ -1,10 +1,12 @@
 import React from "react";
 import StateExample from './useState'
+import ReducerExample from './useReducer'
 
 function stateMainfile() {
     return(
         <div>
         <StateExample />
+        <ReducerExample />
         </div>
     );
 }
