998e514393c7e338c7315c02a5ca47ab9e26c950
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-02 22:28:49 +0500

    Forms: HandleFormUsingControlledForm Example

--- react-app/src/Components/Forms/formMain.js
+++ react-app/src/Components/Forms/formMain.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ControlledForm from "./controlledForm";
 import UnControlledForm from "./unControlledForm";
+import ContactForm from "./handleFormUsingControlled";
 
 function FormMain () {
     return (
         <div>
             <ControlledForm />
             <UnControlledForm />
+            <ContactForm />
         </div>
     );
 }
