8e2a83384e3d5336acc5f9cc5c709a9c374123f9
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-02 20:50:39 +0500

    Event Handler : Inline Example

---
+++ react-app/src/Components/EventHandling/inlineEventHandler.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Inline() {
+  return (
+    <div>
+      <button onClick={() => alert('Button clicked!')}>Click me</button>
+    </div>
+  );
+}
+
+export default Inline;
