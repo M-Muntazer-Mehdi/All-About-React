0e36493de2bdc9fd45906f9b36b61cd86b0cf53f
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-02 17:08:48 +0500

    Component Lifecycle: Mounting Example

---
+++ react-app/src/Components/componentLifecycle/Lifecycle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Mounting from "./Mounting"
+
+function Lifecycle(){
+    return(
+        <div>
+        <Mounting />
+        </div>
+    );
+}
+
+export default Lifecycle;
