b62a13e617becfbfdc5e445450acec3d3859eca3
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-06 00:00:11 +0500

    Hoc: Create the Protected Wrap Component

---
+++ react-app/src/Components/higherOrderComponent/testFile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WrapFile = () => {
+    return(
+        <div>
+            <h4>Hoc Wrapped Component</h4>
+            <p>In this, We Wrap the testfile with Higher order component with the use of a login page</p>
+        </div>
+    );
+};
+
+export default WrapFile;
