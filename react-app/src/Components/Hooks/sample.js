1d805723eed0e5dfd3d4404727461c3aea81aaa2
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-10-21 17:43:23 +0500

    useMemo - Sample Product List

---
+++ react-app/src/Components/Hooks/sample.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const sampleProducts = [
+    { id: 1, name: 'Apple iPhone 14', category: 'smartphones', price: 999, brand: 'Apple' },
+    { id: 2, name: 'Samsung Galaxy S22', category: 'smartphones', price: 799, brand: 'Samsung' },
+    { id: 3, name: 'Sony WH-1000XM4', category: 'headphones', price: 349, brand: 'Sony' },
+    { id: 4, name: 'Dell XPS 13', category: 'laptops', price: 1299, brand: 'Dell' },
+    { id: 5, name: 'Apple MacBook Pro', category: 'laptops', price: 1999, brand: 'Apple' },
+  ];
+
+export default sampleProducts;
