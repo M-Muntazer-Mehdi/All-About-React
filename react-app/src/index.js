30c468c90fa14eca2e54f708a12e09d5f1587f7d
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-07-31 14:25:51 +0500

    Redux - provide store to React

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import provider from 'react-redux';
+import store from './Components/Redux/plainRedux/store';
 import App from './App';
 
 //Update the main entry point
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </provider>
 );
