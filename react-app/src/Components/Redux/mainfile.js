fae99afd8c8b53648fc80d45990515730abb88dd
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-07-31 17:20:43 +0500

    Redux - Redux Tookit explain

--- react-app/src/Components/Redux/mainfile.js
+++ react-app/src/Components/Redux/mainfile.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Plainredux from "./plainRedux";
 import ReduxThunk from "./thunkRedux";
+import RKTRedux from "./rktRedux";
 
 function MainFile (){
     return(
         <div>
             <Plainredux />
             <ReduxThunk />
+            <RKTRedux />
         </div>
     );
 }
